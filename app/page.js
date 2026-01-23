d18db581e1ce1558d523bb3c97257727718b38a4
dahboz <festusdabere@gmail.com>
2026-01-23 11:57:18 +0100

    First commit

---
+++ app/page.js
@@ -0,0 +1,9 @@
+export default function Home () {
+  return (
+      <main>
+        <div>
+          <p className="text-4xl text-blue-">welcome to my home page</p>
+        </div>
+      </main>
+  )
+}
